e70ffa30d5fdeb7c7ca09be05b734e166c1fc422
Alvaro Bartolome <36760800+alvarobartt@users.noreply.github.com>
2025-03-20 18:25:49 +0100

    Add `log_{info,warn,debug,error}_with_time`

--- log.h
+++ log.h
@@ -3,6 +3,8 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <stdbool.h>
+#include <time.h>
 
 #ifdef _WIN32
 #    define LOG_NEWLINE "\r\n"
@@ -26,24 +28,43 @@ static const char* log_level_to_string(LogLevel lvl) {
     }
 }
 
-static inline void log_print(LogLevel lvl, const char *fmt, ...)
-    __attribute__((format(printf, 2, 3)));
+static inline void log_print(LogLevel lvl, bool tm, const char *fmt, ...)
+    __attribute__((format(printf, 3, 4)));
 
-static inline void log_print(LogLevel lvl, const char *fmt, ...) {
+static inline void log_print(LogLevel lvl, bool tm, const char *fmt, ...) {
     va_list args;
     va_start(args, fmt);
 
     FILE* stream = (lvl == LOG_LEVEL_ERROR) ? stderr : stdout;
-    fprintf(stream, "[%s] ", log_level_to_string(lvl));
+    fprintf(stream, "[%5s] - ", log_level_to_string(lvl));
+
+    if (tm == true) {
+        time_t current_time;
+        struct tm *time_info;
+        char time_string[20];
+
+        time(&current_time);
+        time_info = localtime(&current_time);
+        strftime(time_string, sizeof(time_string), "%Y-%m-%d %H:%M:%S", time_info);
+        fprintf(stream, "%19s - ", time_string);
+    }
+
     vfprintf(stream, fmt, args);
     fputs(LOG_NEWLINE, stream);
 
     va_end(args);
 }
 
-#define log_info(fmt, ...)  log_print(LOG_LEVEL_INFO, fmt, ##__VA_ARGS__)
-#define log_warn(fmt, ...)  log_print(LOG_LEVEL_WARN, fmt, ##__VA_ARGS__)
-#define log_debug(fmt, ...) log_print(LOG_LEVEL_DEBUG, fmt, ##__VA_ARGS__)
-#define log_error(fmt, ...) log_print(LOG_LEVEL_ERROR, fmt, ##__VA_ARGS__)
+#define log_info(fmt, ...)  log_print(LOG_LEVEL_INFO, false, fmt, ##__VA_ARGS__)
+#define log_info_with_time(fmt, ...)  log_print(LOG_LEVEL_INFO, true, fmt, ##__VA_ARGS__)
+
+#define log_warn(fmt, ...)  log_print(LOG_LEVEL_WARN, false, fmt, ##__VA_ARGS__)
+#define log_warn_with_time(fmt, ...)  log_print(LOG_LEVEL_WARN, true, fmt, ##__VA_ARGS__)
+
+#define log_debug(fmt, ...) log_print(LOG_LEVEL_DEBUG, false, fmt, ##__VA_ARGS__)
+#define log_debug_with_time(fmt, ...) log_print(LOG_LEVEL_DEBUG, true, fmt, ##__VA_ARGS__)
+
+#define log_error(fmt, ...) log_print(LOG_LEVEL_ERROR, false, fmt, ##__VA_ARGS__)
+#define log_error_with_time(fmt, ...) log_print(LOG_LEVEL_ERROR, true, fmt, ##__VA_ARGS__)
 
 #endif // LOG_H
